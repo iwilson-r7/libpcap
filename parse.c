a10006c1c23c9d4dad7dc63460cbde81c66650b3
Iain Wilson <iain_wilson@rapid7.com>
2019-08-23 12:17:14 +0100

    Return empty string on success or error message on failure

--- parse.c
+++ parse.c
@@ -1,24 +1,50 @@
 #include <emscripten.h>
 
 #include <pcap.h>
+#include <portability.h>
 
 void
 pcap_fmt_errmsg_for_errno(char *errbuf, size_t errbuflen, int errnum, const char *fmt, ...) {
   // No-op, can't link to stdlib for errno with emscripten
 }
 
-int EMSCRIPTEN_KEEPALIVE parse(char *buf) {
+static char last_error[1000];
+
+const char* EMSCRIPTEN_KEEPALIVE parse(char *buf) {
   int snaplen = 65536;
   int linktype = DLT_EN10MB;
 
   struct bpf_program program = { 0 };
 
-  return pcap_compile_nopcap(
-    snaplen,
-    linktype,
+  pcap_t *p;
+
+  int result;
+  const char *ret;
+
+  p = pcap_open_dead(linktype, snaplen);
+
+  if (p == NULL)
+    return "Failed to initialize parser";
+
+  result = pcap_compile(
+    p,
     &program,
     buf,
     0, // No optimization
     0  // Netmask (everything)
   );
+
+  if (result != 0) {
+    pcap_strlcpy(last_error, pcap_geterr(p), sizeof(last_error));
+
+    ret = last_error;
+
+    if (*last_error == 0) {
+      ret = "Unknown error";
+    }
+  }
+
+  pcap_close(p);
+
+  return ret;
 }
